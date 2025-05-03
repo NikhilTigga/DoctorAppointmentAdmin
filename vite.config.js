bd9ad277cd28b33cc586de59b3f9bfa03a97a449
NikhilTigga <146983364+NikhilTigga@users.noreply.github.com>
2025-05-03 22:10:03 +0530

    forth commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173, // Use Render-assigned port if available
-    allowedHosts: ['https://doctorappointmentadmin.onrender.com']
-  }
+  server: { port:5174}
 })
